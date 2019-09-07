42ae33cb36bc3d2b179fd485739a8ef8ba732699
Tobias Casper <tobias.casper@gmail.com>
2019-09-07 13:28:24 +0200

    Add credo, dialyzer, coverage and doc tools

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule TomlConfigProvider.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:ex_unit, :mix]],
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -21,8 +23,11 @@ defmodule TomlConfigProvider.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.1.1", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.20.2", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:toml, "~> 0.5"}
     ]
   end
 end
